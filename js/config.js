bae723519de8cc3f005aa1a699be09853b0949a4
TopCoder <142697530+TopCoderJP@users.noreply.github.com>
2025-01-13 21:17:04 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_p0YJwt0Lm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2hrnq361uj86poql35aochf9b4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_Guvf5VDTU', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '72i9e87mhp3hrjjcj8flp9kcf4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
